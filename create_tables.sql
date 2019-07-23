05b75a5a97d2fbe26680af9a7ae03eee6ff440fc
ishidafish <51567618+ishidafish@users.noreply.github.com>
2019-07-23 17:56:19 +0800

    trandata logs all
    messages those subscribe from MQTT

---
+++ create_tables.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE `pabox`;
+USE `pabox`;
+
+CREATE TABLE `code_detail` (
+  `code_type` varchar(32) DEFAULT NULL,
+  `code` varchar(45) DEFAULT NULL,
+  `code_name` varchar(45) DEFAULT NULL,
+  `desc` varchar(45) DEFAULT NULL,
+  `attr01` varchar(45) DEFAULT NULL,
+  `attr02` varchar(45) DEFAULT NULL,
+  `attr03` varchar(45) DEFAULT NULL,
+  `attr04` varchar(45) DEFAULT NULL,
+  `attr05` varchar(45) DEFAULT NULL,
+  UNIQUE KEY `code_UNIQUE` (`code_type`,`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT NULL,
+  `iot_id` varchar(45) DEFAULT NULL,
+  `desc` varchar(45) DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  `last_off_time` datetime DEFAULT NULL,
+  `on_time` datetime DEFAULT NULL,
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `iot_id_UNIQUE` (`iot_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `trandata` (
+  `line_id` int(11) NOT NULL AUTO_INCREMENT,
+  `iot_id` varchar(32) DEFAULT NULL,
+  `code` varchar(45) DEFAULT NULL,
+  `code_name` varchar(45) DEFAULT NULL,
+  `seq` int(11) DEFAULT NULL,
+  `desc` varchar(45) DEFAULT NULL,
+  `data` varchar(200) DEFAULT NULL,
+  `attr01` varchar(45) DEFAULT NULL,
+  `attr02` varchar(45) DEFAULT NULL,
+  `attr03` varchar(45) DEFAULT NULL,
+  `attr04` varchar(45) DEFAULT NULL,
+  `attr05` varchar(45) DEFAULT NULL,
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`line_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
